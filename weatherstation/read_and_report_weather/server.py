870c5b3b3319e6a47094a26ec339da86e739ca26
andressl91 <pep8@pm.me>
2018-11-09 13:13:17 +0100

    Starting to make it right

--- weatherstation/read_and_report_weather/server.py
+++ weatherstation/read_and_report_weather/server.py
@@ -4,30 +4,47 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 10000
 #server_address = ('localhost', port)
-server_address = ('192.168.20.20', port)
+server_address = ('192.168.20.6', port)
 sock.bind(server_address)
 print(f"Starting server, listning on port {port}")
 sock.listen(1)
 
+data_delivery = []
 while True:
     # Wait for a connect
 
     connection, client_address = sock.accept()
-    
-    try:
-        print(f"Connection from {connection}")
+    if connection:
+        print(f"{client_address} says hello")
+        try:
+            print(f"Connection from {connection}")
 
-        while True:
-            data = connection.recv(2**4)
-            print(f"Recieved {data}")
+            while True:
+                data = connection.recv(2**4)
+                # data_delivery.append(data)
+                
+                if data.decode('utf-8') == "S1":
+                    
+                    while data.decode('utf-8') != "End":
+                        print(f"Recieved {data}")
+                        data = connection.recv(2**4)
+                        data_delivery.append(data.decode('utf-8'))
+                        print(data_delivery)
+                    
+                    print("All sensor data recieved")
+                    data_delivery = []
 
-            if data:
-                print("Sending data back to client")
-                connection.sendall(data)
-            else:
-                print("No more data from client")
-                break
-    finally:
-        # Clean up the connection
-        connection.close()
+                #    print("Sending data back to client")
+                #    connection.send(b"Got your stuff")
+                    # connection.sendall(data)
+
+                #    print(data_delivery)
+
+                else:
+                    print("No more data from client")
+                    break
+        finally:
+            # Clean up the connection
+            # connection.close()
+            print("Finally")
 
