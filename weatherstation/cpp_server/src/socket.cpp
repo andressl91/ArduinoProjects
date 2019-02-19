26a557c2d8681c516bc6974d54d1acf5584481c7
andressl91 <pep8@pm.me>
2019-02-19 17:22:11 +0100

    Updated cmakelist

--- weatherstation/cpp_server/src/socket.cpp
+++ weatherstation/cpp_server/src/socket.cpp
@@ -2,16 +2,10 @@
 #include <string>
 #include "sensor_collector.h"
 
-void startServer() {
-    SensorServer serv(1234);
-    serv.startServerIO();
-}
 
 int main() {
-    //startServer();    
+    SensorServer serv(1234);
+    serv.startServerIO();
     std::string db_path = "mydb.db";
-    //SqliteConnection sql(db_path);
-    //sql.openDB();
-    //sql.closeDB();
     return 0;
 }
